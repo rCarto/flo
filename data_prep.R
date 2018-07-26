23c3950a3605a868cffe2d6cd52c554367803193
antuki <kim.demarty@laposte.net>
2018-07-26 22:59:19 +0200

    commit kim 1

--- data_prep.R
+++ data_prep.R
@@ -15,7 +15,6 @@ iris_75 <- iris_fra[substr(iris_fra$INSEE_COM, 1,2)=="75",]
 iris_75 <- merge(iris_75, iris_data, by.x = "CODE_IRIS", by.y = "IRIS", all.x = T)
 iris_75 <- iris_75[,c("CODE_IRIS", "P14_POP")]
 
-
 iris_p13 <- iris_fra[iris_fra$INSEE_COM=="75113",]
 iris_p13 <- merge(iris_p13, iris_data, by.x = "CODE_IRIS", by.y = "IRIS", all.x = T)
 iris_p13 <- iris_p13[,c("CODE_IRIS", "P14_POP")]
@@ -23,6 +22,10 @@ iris_p13 <- iris_p13[,c("CODE_IRIS", "P14_POP")]
 iris_p14 <- iris_fra[iris_fra$INSEE_COM=="75114",]
 iris_p14 <- merge(iris_p14, iris_data, by.x = "CODE_IRIS", by.y = "IRIS", all.x = T)
 iris_p14 <- iris_p14[,c("CODE_IRIS", "P14_POP")]
+
+iris_31 <- iris_fra[substr(iris_fra$INSEE_COM, 1,2)=="31",]
+iris_31 <- merge(iris_31, iris_data, by.x = "CODE_IRIS", by.y = "IRIS", all.x = T)
+iris_31 <- iris_31[,c("CODE_IRIS", "P14_POP","INSEE_COM")]
 
 iris_tlse <- iris_fra[iris_fra$INSEE_COM=="31555",]
 iris_tlse <- merge(iris_tlse, iris_data, by.x = "CODE_IRIS", by.y = "IRIS", all.x = T)
@@ -66,18 +69,20 @@ sir_p14 <- st_transform(sir_p14, 2154)
 sir_p14 <- st_jitter(sir_p14, 1)
 
 
-sir_tlse <- read.csv('data-raw/geo-sirene_31.csv', stringsAsFactors = F)
-sir_tlse <- sir_tlse[substr(sir_tlse$APET700,1,4)==5610,]
-sir_tlse <- st_as_sf(sir_tlse, coords=c("longitude", "latitude"), crs = 4326, 
-                     stringsAsFactors = FALSE)
-sir_tlse <- st_transform(sir_tlse, 2154)
-sir_tlse <- st_jitter(sir_tlse, 1)
-sir_tlse <- sir_tlse[sir_tlse$COMET==555,]
+sir_31 <- read.csv('data-raw/geo-sirene_31.csv', stringsAsFactors = F)
+sir_31 <- sir_31[substr(sir_31$APET700,1,4)==5610,]
+sir_31 <- st_as_sf(sir_31, coords=c("longitude", "latitude"), crs = 4326, 
+                   stringsAsFactors = FALSE)
+sir_31 <- st_transform(sir_31, 2154)
+sir_31 <- st_jitter(sir_31, 1)
+sir_tlse <- sir_31[sir_31$COMET==555,]
 
 
 
 ####### OSM extact
 library(osmdata)
+
+#osm (75)
 q <- opq(bbox=st_bbox(st_transform(iris_75, 4326)))
 q1 <- add_osm_feature(opq = q, key = 'amenity', 
                       value = "restaurant" )
@@ -102,9 +107,6 @@ res <- osmdata_sf(q1)
 cafe1  <- res$osm_points[!is.na(res$osm_points$amenity),]
 cafe2 <- res$osm_polygons
 st_geometry(cafe2) <- st_centroid(st_geometry(cafe2))
-
-
-
 osm <- rbind(restau1[, c("osm_id", "name")], restau2[, c("osm_id", "name")], 
              restau3[, c("osm_id", "name")], fast1[, c("osm_id", "name")], 
              fast2[, c("osm_id", "name")], 
@@ -114,7 +116,7 @@ osm <- st_intersection(osm, iris_75)
 osm <- st_jitter(osm, 1)
 
 
-
+#osm_p13
 q <- opq(bbox=st_bbox(st_transform(iris_p13, 4326)))
 q1 <- add_osm_feature(opq = q, key = 'amenity', 
                       value = "restaurant" )
@@ -142,7 +144,45 @@ osm_p13 <- st_intersection(osm_p13, iris_p13)
 oms_p13 <- st_jitter(osm_p13, 1)
 
 
+#osm_31
+q <- opq(bbox=st_bbox(st_transform(iris_31, 4326)))
+q1 <- add_osm_feature(opq = q, key = 'amenity', 
+                      value = "restaurant" )
+res <- osmdata_sf(q1)
+restau1  <- res$osm_points[!is.na(res$osm_points$amenity),]
+restau2 <- res$osm_polygons
+st_geometry(restau2) <- st_centroid(st_geometry(restau2))
+restau3 <- res$osm_multipolygons
+st_geometry(restau3) <- st_centroid(st_geometry(restau3), 
+                                    of_largest_polygon = TRUE)
 
+q1 <- add_osm_feature(opq = q, key = 'amenity', 
+                      value = "fast_food" )
+res <- osmdata_sf(q1)
+fast1  <- res$osm_points[!is.na(res$osm_points$amenity),]
+fast2 <- res$osm_polygons
+st_geometry(fast2) <- st_centroid(st_geometry(fast2))
+
+q1 <- add_osm_feature(opq = q, key = 'amenity', 
+                      value = "cafe" )
+res <- osmdata_sf(q1)
+cafe1  <- res$osm_points[!is.na(res$osm_points$amenity),]
+cafe2 <- res$osm_polygons
+st_geometry(cafe2) <- st_centroid(st_geometry(cafe2))
+
+
+osm_31 <- rbind(restau1[, c("osm_id", "name")], restau2[, c("osm_id", "name")], 
+             restau3[, c("osm_id", "name")], fast1[, c("osm_id", "name")], 
+             fast2[, c("osm_id", "name")], 
+             cafe1[, c("osm_id", "name")], cafe2[, c("osm_id", "name")])
+osm_31 <- st_transform(osm_31, 2154)
+osm_31 <- st_intersection(osm_31, iris_31)
+osm_31 <- st_jitter(osm_31, 1)
+
+
+
+
+#osm_p14
 q <- opq(bbox=st_bbox(st_transform(iris_p14, 4326)))
 q1 <- add_osm_feature(opq = q, key = 'amenity', 
                       value = "restaurant" )
@@ -170,6 +210,7 @@ osm_p14 <- st_intersection(osm_p14, iris_p14)
 oms_p14 <- st_jitter(osm_p14, 1)
 
 
+#osm_tlse
 q <- opq(bbox=st_bbox(st_transform(iris_tlse, 4326)))
 q1 <- add_osm_feature(opq = q, key = 'amenity', 
                       value = "restaurant" )
@@ -196,13 +237,20 @@ osm_tlse <- st_transform(osm_tlse, 2154)
 osm_tlse <- st_intersection(osm_tlse, iris_tlse)
 osm_tlse <- st_jitter(osm_tlse, 1)
 
-
-
+####### tables de passage supra-communales (effet MAUP)
+#devtools::install_github("antuki/COGugaison)
+library(COGugaison)
+library(dplyr)
+table_MAUP <- table_supracom_2017 %>%
+  filter(DEP=="31") %>% 
+  select(-DEP,-REG,-NATURE_EPCI,-TUU2014,-TDUU2014,-TAU2014,-CATAEU2010)
+  
 
 
 #######  Export
 saveRDS(object = iris_p13, file = "data/iris_p13.rds")
 saveRDS(object = iris_p14, file = "data/iris_p14.rds")
+saveRDS(object = iris_31, file = "data/iris_31.rds")
 saveRDS(object = iris_tlse, file = "data/iris_tlse.rds")
 saveRDS(object = iris_75, file = "data/iris_75.rds")
 
@@ -210,12 +258,15 @@ saveRDS(object = iris_75, file = "data/iris_75.rds")
 
 saveRDS(object = sir_p13, file = "data/sir_p13.rds")
 saveRDS(object = sir_p14, file = "data/sir_p14.rds")
+saveRDS(object = sir_31, file = "data/sir_31.rds")
 saveRDS(object = sir_tlse, file = "data/sir_tlse.rds")
 saveRDS(object = sir, file = "data/sir.rds")
 
 
 saveRDS(object = osm_p13, file = "data/osm_p13.rds")
 saveRDS(object = osm_p14, file = "data/osm_p14.rds")
+saveRDS(object = osm_31, file = "data/osm_31.rds")
 saveRDS(object = osm_tlse, file = "data/osm_tlse.rds")
 saveRDS(object = osm, file = "data/osm.rds")
 
+saveRDS(object = table_MAUP, file = "data/table_MAUP.rds")
